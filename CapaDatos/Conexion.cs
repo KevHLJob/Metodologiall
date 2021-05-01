0ff64a79eedad3533735a0b0eede084a07734277
KevinHackNinja <61767060+KevinHackNinja@users.noreply.github.com>
2021-04-30 20:40:15 -0600

    Add files via upload

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=LAPTOP-T7KQ9T54\\SQLEXPRESS;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+    }
+}
